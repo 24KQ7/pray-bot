32edc5091ea95854d548f44daf427b5d2f714c36
24KQ7 <alqtaishat.p@gmail.com>
2026-06-13 09:25:37 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -9,16 +9,16 @@
 
 const CONFIG = {
   // Bot invite link - users will be redirected here when clicking "Invite Kali Billi"
-  botInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=YOUR_BOT_CLIENT_ID&permissions=8&scope=bot",
+  botInviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1512469723848249374&permissions=8&scope=bot",
   
   // Support server invite link - users will be redirected here for support
-  supportServerUrl: "https://discord.gg/YOUR_SERVER_INVITE_CODE",
+  supportServerUrl: "https://discord.gg/s7DyKBUqVh",
   
   // Bot name (for display)
-  botName: "Kali Billi",
+  botName: "Pray",
   
   // Bot description
-  botDescription: "Your Cute Discord Companion - Fun, moderation, and AI chat for your Discord server."
+  botDescription: "Your All in one Discord Asistant - Fun, moderation, and AI chat for your Discord server."
 };
 
 // Export for use in browsers
